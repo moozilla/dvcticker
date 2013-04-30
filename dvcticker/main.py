5081ab1e5290c385c27adf9a6d9bee41b8a41da0
moozilla <evilgeniuscow@gmail.com>
2013-04-29 21:49:31 -0700

    Timeout errors should now be handled correctly. Also images are now dynamically sized.

--- dvcticker/main.py
+++ dvcticker/main.py
@@ -7,6 +7,7 @@ import StringIO
 import jinja2
 import os
 from decimal import * #used fixed point math for better accuracy
+from google.appengine import runtime # for catching DeadlineExceededError
 
 JINJA_ENVIRONMENT = jinja2.Environment(
     loader=jinja2.FileSystemLoader(os.path.dirname(__file__)))
@@ -18,17 +19,19 @@ def urlfetch_cache(url):
         return data
     else:
         try:
-            result = urlfetch.fetch(url,deadline=10) #timeout after 10 sec
+            result = urlfetch.fetch(url,deadline=30) #timeout after 10 sec
             if result.status_code == 200 and result.content != '"Unknown currency"':
                 obj = json.loads(result.content) #should probably add error handling in case bad json is passed
                 value = obj['value']
-                memcache.add(url, value, 15) #cache for 15 sec
+                memcache.add(url, value, 30) #cache for 30 sec
                 return value
             else:
                 return 'Error'#'Error accessing Vircurex API'
-        except DeadlineExceededError:
+        except runtime.DeadlineExceededError: #raised if the overall request times out
             return 'Error: timeout'
-        except DownloadError:
+        except runtime.apiproxy_errors.DeadlineExceededError: #raised if an RPC exceeded its deadline (set)
+            return 'Error: timeout'
+        except google.appengine.api.urlfetch_errors.DeadlineExceededError: #raised if the URLFetch times out
             return 'Error: timeout'
     
 def get_vircurex_json(type, base, alt):
@@ -65,13 +68,12 @@ class MainHandler(webapp2.RequestHandler):
         
 class ImageHandler(webapp2.RequestHandler):
     def get(self,exchange,amount,base,alt):
-        if amount == '': amount = '1'   # default amount is 1
+        if amount == '': amount = '1'       # default amount is 1
         if alt == None:
             if base == 'btc': alt = 'usd'   # btc.png just shows btc value in usd
             else: alt = 'btc'               # if no alt specified, default to BTC
         value = str(Decimal(amount)*Decimal(get_vircurex_json('bid',base,alt)))
-        width = 120 # should be determined programatically
-        text_pos = 19   # 3 px after coin image (all are 16x16)
+        text_pos = 19                       # 3 px after coin image (all are 16x16)
         
         if alt == 'usd':
             # round down to 2 decimal places
@@ -80,21 +82,23 @@ class ImageHandler(webapp2.RequestHandler):
         if alt == 'eur':
             # euro symbol in unicode (only works with truetype fonts)
             value = u'\u20AC '+str(Decimal(value).quantize(Decimal('.01'), rounding=ROUND_DOWN))
-            text_pos = 2                # have to position euro symbol so it doesn't cut off
-        if alt == 'dvc':
-            width = 160     # bigger width for dvc because it tends to have more digits
+            text_pos = 2                    # have to position euro symbol so it doesn't cut off
         if value == 'Error':
             text_pos = 0
         
-        img = Image.new("RGBA", (width,20))
-        if any(alt in s for s in ['btc', 'dvc', 'ixc', 'ltc', 'nmc', 'ppc', 'trc']):
-            coinimg = Image.open('static/img/'+alt+'.png') 
-            img.paste(coinimg, (0,2)) #paste the coin image into the generated image
-            
+        img = Image.new("RGBA", (1,1))      # just used to calculate the text size, size doesn't matter
         draw = ImageDraw.Draw(img)
         #fnt = ImageFont.load('static/font/ncenB12.pil') # for testing locally, can't get truetype to work locally
         fnt = ImageFont.truetype('static/font/tahoma_bold.ttf', 14, encoding='unic')
-        draw.text((text_pos,1), value, font=fnt, fill='#333333')
+        w, h = draw.textsize(value, fnt)    # calculate width font will take up
+        
+        img = Image.new("RGBA", (w+text_pos,20))
+        draw = ImageDraw.Draw(img)          # set draw to new image
+        if any(alt in s for s in ['btc', 'dvc', 'ixc', 'ltc', 'nmc', 'ppc', 'trc']):
+            coinimg = Image.open('static/img/'+alt+'.png') 
+            img.paste(coinimg, (0,2))       #paste the coin image into the generated image
+        
+        draw.text((text_pos,1), value, font=fnt, fill='#555555')
         del draw
         
         output = StringIO.StringIO()
